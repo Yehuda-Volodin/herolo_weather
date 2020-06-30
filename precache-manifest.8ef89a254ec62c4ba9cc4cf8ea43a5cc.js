dbdcdf89501d232d054bf0790aee2c6d31effccf
Yehuda Volodin <falcon81@tut.by>
2020-06-30 18:52:27 +0300

    Updates

---
+++ precache-manifest.8ef89a254ec62c4ba9cc4cf8ea43a5cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719888b0d4623e85e0743b01223bf89f",
+    "url": "/herolo_weather/index.html"
+  },
+  {
+    "revision": "2fc838f843a3879c163b",
+    "url": "/herolo_weather/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ab5cadd5317c7e7dc7ab",
+    "url": "/herolo_weather/static/css/main.6de27c54.chunk.css"
+  },
+  {
+    "revision": "2fc838f843a3879c163b",
+    "url": "/herolo_weather/static/js/2.03471ba5.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/herolo_weather/static/js/2.03471ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab5cadd5317c7e7dc7ab",
+    "url": "/herolo_weather/static/js/main.123ce2ca.chunk.js"
+  },
+  {
+    "revision": "01c552f75b7582024c7a",
+    "url": "/herolo_weather/static/js/runtime-main.d81d312f.js"
+  },
+  {
+    "revision": "dc9ed497c447bd5ac07f5fe84fe162ee",
+    "url": "/herolo_weather/static/media/weather_img.dc9ed497.jpg"
+  },
+  {
+    "revision": "128e6614bd6df0ae5d294ecf0cab20c6",
+    "url": "/herolo_weather/static/media/weather_light_img.128e6614.jpg"
+  }
+]);
